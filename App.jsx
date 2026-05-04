b853640214a961bd7bce886791114ab7365e773b
VELLA <kourtneyferguson@hotmail.com>
2026-05-04 12:52:00 +1000

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,3 +1,10 @@
 return (
-  <h1>Hello Vella!</h1>
+  <div>
+    <h1>Vella</h1>
+    <p>Save your favourite videos, organise them into folders, and set reminders.</p>
+
+    <button>My Saved Videos</button>
+    <button>My Planner</button>
+    <button>Create Folder</button>
+  </div>
 )
